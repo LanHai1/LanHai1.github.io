9ba3593bb3783b7c7ccb0b66e7ea7aa376945337
lanhai <1444133004@qq.com>
2019-05-25 10:45:29 +0800

    snake touch over

--- snake/game.js
+++ snake/game.js
@@ -159,6 +159,73 @@
             }
         }
 
+        var startx, starty;
+        //获得角度
+        function getAngle(angx, angy) {
+            return Math.atan2(angy, angx) * 180 / Math.PI;
+        };
+
+        //根据起点终点返回方向 1向上 2向下 3向左 4向右 0未滑动
+        function getDirection(startx, starty, endx, endy) {
+            var angx = endx - startx;
+            var angy = endy - starty;
+            var result = 0;
+
+            //如果滑动距离太短
+            if (Math.abs(angx) < 2 && Math.abs(angy) < 2) {
+                return result;
+            }
+
+            var angle = getAngle(angx, angy);
+            if (angle >= -135 && angle <= -45) {
+                result = 1;
+            } else if (angle > 45 && angle < 135) {
+                result = 2;
+            } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+                result = 3;
+            } else if (angle >= -45 && angle <= 45) {
+                result = 4;
+            }
+
+            return result;
+        }
+        //手指接触屏幕
+        document.addEventListener("touchstart", function(e) {
+            startx = e.touches[0].pageX;
+            starty = e.touches[0].pageY;
+            e.preventDefault()
+        }, false);
+        //手指离开屏幕
+        document.addEventListener("touchend", function(e) {
+            var endx, endy;
+            endx = e.changedTouches[0].pageX;
+            endy = e.changedTouches[0].pageY;
+            var direction = getDirection(startx, starty, endx, endy);
+            e.preventDefault()
+            switch (direction) {
+                case 1:
+                    if (that.snake.direction != "bottom") {
+                        that.snake.direction = "top"
+                    }
+                    break;
+                case 2:
+                    if (that.snake.direction != "top") {
+                        that.snake.direction = "bottom"
+                    }
+                    break;
+                case 3:
+                    if (that.snake.direction != "right") {
+                        that.snake.direction = "left"
+                    }
+                    break;
+                case 4:
+                    if (that.snake.direction != "left") {
+                        that.snake.direction = "right"
+                    }
+                    break;
+            }
+        }, false);
+
     }
 
     Game.prototype.userStorage = function() {
