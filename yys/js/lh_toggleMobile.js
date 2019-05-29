7de13bc81b75cdd77db735cf6b58886557420c0d
lanhai <1444133004@qq.com>
2019-05-29 20:15:32 +0800

    阴阳师index

---
+++ yys/js/lh_toggleMobile.js
@@ -0,0 +1,10 @@
+let mobile_url = "index1.html";
+var isMobile = false;
+// 检测userAgent
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    isMobile = true;
+}
+// 跳转至手机端
+if (isMobile) {
+    window.location.href = mobile_url;
+}
