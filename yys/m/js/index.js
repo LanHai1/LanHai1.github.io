6dd9cd22d7fbef3dbe41a5641cea538efd7dd400
lanhai <1444133004@qq.com>
2019-05-30 01:34:51 +0800

    阴阳师 移动端

--- yys/m/js/index.js
+++ yys/m/js/index.js
@@ -9,7 +9,41 @@ $(function() {
     })
 
     // 新闻资讯
+    let swiperIndex = 0;
     $(".m_next_news").click(function() {
-        console.log($(".m_swiper-slide").width())
+        swiperIndex++;
+        if (swiperIndex != 0) {
+            $(".m_prev_news").animate({
+                opacity: 1
+            }).css("pointer-events", "auto")
+        }
+        if (swiperIndex == 2) {
+            $(".m_next_news").animate({
+                opacity: 0
+            }).css("pointer-events", "none")
+        }
+        let swiperW = $(".m_swiper-slide").width();
+        $(".m_newswrap").animate({
+            left: -(swiperIndex * swiperW)
+        })
     })
+
+    $(".m_prev_news").click(function() {
+        swiperIndex--;
+        if (swiperIndex >= 1) {
+            $(".m_next_news").animate({
+                opacity: 1
+            }).css("pointer-events", "auto")
+        }
+        if (swiperIndex == 0) {
+            $(".m_prev_news").animate({
+                opacity: 0
+            }).css("pointer-events", "none")
+        }
+        let swiperW = $(".m_swiper-slide").width();
+        $(".m_newswrap").animate({
+            left: -(swiperIndex * swiperW)
+        })
+    })
+
 })
