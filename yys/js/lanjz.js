aa3aaeb32dc489f2be856c5ae557a774958c8f1e
lanhai <1444133004@qq.com>
2019-06-02 11:40:06 +0800

    阴阳师 首页 PC 懒加载

---
+++ yys/js/lanjz.js
@@ -0,0 +1,50 @@
+// 图片懒加载类
+var LazyLoads = /** @class */ (function () {
+    // 构造器
+    function LazyLoads(el) {
+        var _this = this;
+        // 判断是否该图片是否可以加载
+        this.canILoad = function () {
+            // 循环遍历数组
+            for (var i = _this.imglist.length; i--;) {
+                _this.getBound(_this.imglist[i]) && _this.loadImage(_this.imglist[i], i);
+            }
+        };
+        // 获取图片与窗口的信息
+        this.getBound = function (el) {
+            // 获取元素相对于视窗的位置
+            var bound = el.getBoundingClientRect();
+            // 获取window可视高度
+            var clientHeight = window.innerHeight;
+            // 判断是否需要加载数据
+            return (bound.top <= clientHeight);
+        };
+        // 加载图片
+        this.loadImage = function (el, index) {
+            // 获取自定义属性
+            var src = el.getAttribute('data-lanhai');
+            // 赋值
+            el.src = src;
+            // 删除已经渲染了的图片
+            _this.imglist.splice(index, 1);
+        };
+        this.getScroll = function () {
+            // 为浏览器注册滚动时间 监测是否继续懒加载
+            window.addEventListener('scroll', function () {
+                _this.imglist.length && _this.canILoad();
+            });
+        };
+        // 初始化
+        this.init = function () {
+            _this.canILoad();
+            _this.getScroll();
+        };
+        // 获取图片元素 追加至数组
+        this.imglist = Array.from(document.querySelectorAll(el)); // 需使用懒加载的图片集合
+    }
+    return LazyLoads;
+}());
+// 实例化对象，参数则是需要使用懒加载的图片类名
+var start = new LazyLoads('img');
+// 初始化
+start.init();
---
+++ yys/js/lanjz.ts
@@ -0,0 +1,51 @@
+// 图片懒加载类
+class LazyLoads {
+    imglist;
+    // 构造器
+    constructor(el) {
+        // 获取图片元素 追加至数组
+        this.imglist = Array.from(document.querySelectorAll(el)); // 需使用懒加载的图片集合
+    }
+
+    // 判断是否该图片是否可以加载
+    canILoad = () => {
+        // 循环遍历数组
+        for (let i = this.imglist.length; i--;) {
+            this.getBound(this.imglist[i]) && this.loadImage(this.imglist[i], i);
+        }
+    }
+    // 获取图片与窗口的信息
+    getBound = (el) => {
+        // 获取元素相对于视窗的位置
+        let bound = el.getBoundingClientRect();
+        // 获取window可视高度
+        let clientHeight = window.innerHeight;
+        // 判断是否需要加载数据
+        return (bound.top <= clientHeight);
+    }
+    // 加载图片
+    loadImage = (el, index) => {
+        // 获取自定义属性
+        let src = el.getAttribute('data-lanhai');
+        // 赋值
+        el.src = src;
+        // 删除已经渲染了的图片
+        this.imglist.splice(index, 1);
+    }
+
+    getScroll = () => {
+        // 为浏览器注册滚动时间 监测是否继续懒加载
+        window.addEventListener('scroll', () => {
+            this.imglist.length && this.canILoad();
+        });
+    }
+    // 初始化
+    init = () => {
+        this.canILoad();
+        this.getScroll();
+    }
+}
+// 实例化对象，参数则是需要使用懒加载的图片类名
+let start = new LazyLoads('img')
+// 初始化
+start.init()
