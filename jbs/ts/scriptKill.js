6be752b790c687b99e2bf72400d806e1bcaf054b
兰海 <lan.hai@z2a.com.cn>
2021-01-27 00:06:16 +0800

    嗯嗯大西瓜

--- jbs/ts/scriptKill.js
+++ jbs/ts/scriptKill.js
@@ -1,14 +1,80 @@
 "use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 var ScriptKill = /** @class */ (function () {
     function ScriptKill(figureArr) {
         this.figureArr = figureArr;
     }
+    // 还原时间 reductionTime("0212",2,":")
+    ScriptKill.prototype.reductionTime = function (soure, start, newStr) {
+        return soure.slice(0, start) + newStr + soure.slice(start);
+    };
     // 生成时间人物样式
     ScriptKill.prototype.generateFigure = function (el) {
-        console.log(this.figureArr, el);
-        this.figureArr.forEach(function (v, i) {
-            el.append("\n            <div class=\"column timeline timelineFigure\" data-figure-name=\"" + v + "\" data-id=\"" + i + "\">\n                <p><img src=\"./img/0" + (i + 1) + ".png\" alt=\"\"></p>\n                <p class='figureName'>" + v + "</p>\n            </div>\n            ");
+        console.log(this.figureArr, el, ScriptKill.renderArr);
+        // this.figureArr.forEach((v, i) => {
+        //     el.append(`
+        //     <div class="column timeline timelineFigure figure${i + 1}" data-figure-name="${v}" data-id="${i + 1}">
+        //         <p><img src="./img/0${i + 1}.png" alt=""></p>
+        //         <p class='figureName'>${v}</p>
+        //         <div class="addPlus"><i class="plus circle icon"></i></div>
+        //     </div>
+        //     `)
+        // })
+        /**
+         * virtualThead: 虚拟th
+         * virtualTbody: 虚拟tb
+         * recordSizeMax: 存放每个角色任务节点树
+         * maxRecordSize: 最大任务节点数值
+         * storeTime: 存放时间
+         */
+        var _a = ["", "<tr><td>\u7EA6\u7FF0</td><td>\u5DF2\u5BA1\u6838</td><td>None</td>\u00AC</tr>", [], 0, []], virtualThead = _a[0], virtualTbody = _a[1], recordSizeMax = _a[2], maxRecordSize = _a[3], storeTime = _a[4];
+        console.log(ScriptKill.renderArr);
+        // 生成th 
+        ScriptKill.renderArr.forEach(function (v, i) {
+            virtualThead += "\n                <th class=\"tableTh\">\n                    <p><img src=\"./img/0" + (i + 1) + ".png\" alt=\"\"></p>\n                    <p class=\"figureName\">" + v.name + "</p>\n                </th>";
+            recordSizeMax.push(+v.recordSize);
+            if (!i && v.recordSize) {
+                v.dom.forEach(function (v1) {
+                    // 获取时间key
+                    for (var key in v1) {
+                        if (Object.prototype.hasOwnProperty.call(v1, key)) {
+                            storeTime.push(+(key.replace(/:/g, '')));
+                        }
+                    }
+                });
+            }
+            console.log(v.dom);
+            // let virtualTbodyRow = "";
+            // virtualTbody += `
+            //      <tr>
+            //         <td></td>
+            //      </tr>
+            // `;
         });
+        maxRecordSize = Math.max.apply(Math, recordSizeMax);
+        if (maxRecordSize) {
+            // 时间排序 最小=>最大
+            storeTime.sort(function (a, b) { return a - b; });
+            console.log(maxRecordSize, "123", storeTime);
+            for (var index = 0; index < storeTime.length; index++) {
+                console.log(index, storeTime[index]);
+            }
+        }
+        var virtualDom = "\n            <thead><tr class=\"tableThTr\">" + virtualThead + "</tr></thead>\n            <tbody>" + virtualTbody + "</tbody>";
+        $(el).append(virtualDom);
+        // < div class="ui mini input input-group clockpicker" style = "width: 100%;" >
+        //     <input type="text" class="form-control" value = "09:30" placeholder = "请选择时间" >
+        //         <span class="input-group-addon" >
+        //             <span class="glyphicon glyphicon-time" > </span>
+        //                 < /span>
+        //                 < /div>
+        $('.clockpicker').clockpicker();
         // 人物头像点击
         $(".timelineFigure img").on("click", function () {
             var outerLayer = $(this).parent().parent();
@@ -16,6 +82,8 @@ var ScriptKill = /** @class */ (function () {
             console.log(outerLayer.data("id"));
         });
     };
+    // 渲染层
+    ScriptKill.renderArr = [];
     return ScriptKill;
 }());
 // 初始化游戏
@@ -70,7 +138,6 @@ $(function () {
         // 验证 清除无效角色名
         var roleArray = roleName.split("，").filter(function (v) { return v; });
         var flag = roleArray.length == $('#peopleNumber').val();
-        // 预知提前生成人物
         if (flag)
             sk = new ScriptKill(roleArray);
         return flag;
@@ -92,7 +159,13 @@ $(function () {
         $.each(form.serializeArray(), function () {
             map[this.name] = this.value;
         });
-        sk.generateFigure($("#timelines"));
+        // 预知提前生成人物
+        ScriptKill.renderArr = [];
+        // 处理dom渲染组
+        var renderArr = sk.figureArr.map(function (v, i) { return { name: v, dom: [{ "12:12": "测试1" }], recordSize: 1 }; });
+        renderArr.unshift({ name: "时间线", dom: [{ "12:12": "测试1" }, { "02:12": "测试" }], recordSize: 2 });
+        ScriptKill.renderArr = __spreadArrays(renderArr);
+        sk.generateFigure($(".timelineTable"));
         $('.ui.modal').modal('hide');
     });
     // 取消重置
--- jbs/ts/scriptKill.ts
+++ jbs/ts/scriptKill.ts
@@ -7,20 +7,95 @@ interface JBSMap {
 
 class ScriptKill {
     protected figureArr: Array<string | undefined>;
+    // 渲染层
+    static renderArr: Array<any> = [];
     constructor(figureArr: Array<string | undefined>) {
         this.figureArr = figureArr;
     }
+    // 还原时间 reductionTime("0212",2,":")
+    reductionTime(soure: string, start: number, newStr: string) {
+        return soure.slice(0, start) + newStr + soure.slice(start);
+    }
     // 生成时间人物样式
     generateFigure(el: Element) {
-        console.log(this.figureArr, el);
-        this.figureArr.forEach((v, i) => {
-            el.append(`
-            <div class="column timeline timelineFigure" data-figure-name="${v}" data-id="${i}">
-                <p><img src="./img/0${i + 1}.png" alt=""></p>
-                <p class='figureName'>${v}</p>
-            </div>
-            `)
+        console.log(this.figureArr, el, ScriptKill.renderArr);
+        // this.figureArr.forEach((v, i) => {
+        //     el.append(`
+        //     <div class="column timeline timelineFigure figure${i + 1}" data-figure-name="${v}" data-id="${i + 1}">
+        //         <p><img src="./img/0${i + 1}.png" alt=""></p>
+        //         <p class='figureName'>${v}</p>
+        //         <div class="addPlus"><i class="plus circle icon"></i></div>
+        //     </div>
+        //     `)
+        // })
+
+
+        /**
+         * virtualThead: 虚拟th
+         * virtualTbody: 虚拟tb
+         * recordSizeMax: 存放每个角色任务节点树
+         * maxRecordSize: 最大任务节点数值
+         * storeTime: 存放时间
+         */
+        let [virtualThead, virtualTbody, recordSizeMax, maxRecordSize, storeTime]: [string, string, Array<number>, number, Array<number>] = [``, `<tr><td>约翰</td><td>已审核</td><td>None</td>¬</tr>`, [], 0, []]
+        console.log(ScriptKill.renderArr);
+
+        // 生成th 
+        ScriptKill.renderArr.forEach((v: any, i) => {
+            virtualThead += `
+                <th class="tableTh">
+                    <p><img src="./img/0${i + 1}.png" alt=""></p>
+                    <p class="figureName">${v.name}</p>
+                </th>`;
+
+            recordSizeMax.push(+v.recordSize);
+
+            if (!i && v.recordSize) {
+                v.dom.forEach((v1: any) => {
+                    // 获取时间key
+                    for (const key in v1) {
+                        if (Object.prototype.hasOwnProperty.call(v1, key)) {
+                            storeTime.push(+(key.replace(/:/g, '')))
+                        }
+                    }
+                })
+            }
+            console.log(v.dom);
+
+            // let virtualTbodyRow = "";
+            // virtualTbody += `
+            //      <tr>
+            //         <td></td>
+            //      </tr>
+            // `;
         })
+
+        maxRecordSize = Math.max(...recordSizeMax);
+
+        if (maxRecordSize) {
+            // 时间排序 最小=>最大
+            storeTime.sort((a, b) => a - b)
+            console.log(maxRecordSize, "123", storeTime);
+            for (let index = 0; index < storeTime.length; index++) {
+                console.log(index, storeTime[index]);
+            }
+        }
+
+        let virtualDom = `
+            <thead><tr class="tableThTr">${virtualThead}</tr></thead>
+            <tbody>${virtualTbody}</tbody>`;
+
+        $(el).append(virtualDom)
+
+
+        // < div class="ui mini input input-group clockpicker" style = "width: 100%;" >
+        //     <input type="text" class="form-control" value = "09:30" placeholder = "请选择时间" >
+        //         <span class="input-group-addon" >
+        //             <span class="glyphicon glyphicon-time" > </span>
+        //                 < /span>
+        //                 < /div>
+
+        $('.clockpicker').clockpicker();
 
         // 人物头像点击
         $(".timelineFigure img").on("click", function (this: any) {
@@ -84,8 +159,6 @@ $(() => {
         let roleArray = roleName.split("，").filter((v: string | undefined) => v);
 
         const flag = roleArray.length == $('#peopleNumber').val();
-
-        // 预知提前生成人物
         if (flag) sk = new ScriptKill(roleArray);
         return flag;
     }
@@ -111,7 +184,14 @@ $(() => {
             map[this.name] = this.value;
         });
 
-        sk.generateFigure($("#timelines"));
+        // 预知提前生成人物
+        ScriptKill.renderArr = []
+        // 处理dom渲染组
+        const renderArr = sk.figureArr.map((v: string, i: number) => { return { name: v, dom: [{ "12:12": "测试1" }], recordSize: 1 } })
+        renderArr.unshift({ name: "时间线", dom: [{ "12:12": "测试1" }, { "02:12": "测试" }], recordSize: 2 });
+        ScriptKill.renderArr = [...renderArr];
+
+        sk.generateFigure($(".timelineTable"));
 
         $('.ui.modal').modal('hide')
     })
